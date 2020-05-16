662291538e1ee0a93f88ad6e96c19c6e3436d3bf
Sunny Rhoades <smrb2016@mymail.pomona.edu>
2020-05-15 19:04:37 -0700

    adding second path function from machine

--- risk/board.py
+++ risk/board.py
@@ -148,7 +148,15 @@ class Board(object):
         Returns:
             bool: True if the path is an attack path
         '''
-
+        if len(path) < 2:
+	    return False
+        elif self.is_valid_path(path):
+	    for i in range(1, len(path)):
+	        if self.owner(path[i]) == self.owner(path[0]):
+		    return False
+		return True
+        else:
+	    return False
 
     def cost_of_attack_path(self, path):
         '''
